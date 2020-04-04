ae264d60de49a398389e857db599914f63ad706b
Anton Sivagin <ansn.onward@gmail.com>
2020-04-04 20:18:35 +0300

    add hot found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import t from '../../locales/t';
 import './App.css';
 
-function App() {
-  return <p>Not Found</p>;
+class NotFound extends Component {
+  static propTypes = {
+    l: PropTypes.string.isRequired,
+  };
+  render() {
+    const { l } = this.props;
+
+    return (
+      <div>
+        <p>{t('notFound.title', l)}</p>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapState = state => ({
+  l: state.language.language,
+});
+
+export default connect(mapState)(NotFound);
